c7f124fa57653a533317352cdc1a2431b665ee74
Rasinshmek <alphamiha@mail.ru>
2022-04-13 14:51:45 +0300

    swagger bug fixed

--- src/main/java/com/example/ymo/YmoApplication.java
+++ src/main/java/com/example/ymo/YmoApplication.java
@@ -2,13 +2,14 @@ package com.example.ymo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class YmoApplication {
 
     public static void main(String[] args) {
 
         SpringApplication.run(YmoApplication.class, args);
     }
-
 }
--- src/main/java/com/example/ymo/config/SwaggerConfig.java
+++ src/main/java/com/example/ymo/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ class SwaggerConfig {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.ymo"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
