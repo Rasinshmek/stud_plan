8ec6bcd5c37b9f9102b5533e02ba51c2680b5f9b
Rasinshmek <alphamiha@mail.ru>
2023-11-16 10:30:07 +0300

    delete 2 rows

--- src/main/java/com/example/ymo/load/OtherServices.java
+++ src/main/java/com/example/ymo/load/OtherServices.java
@@ -1,6 +1,7 @@
 package com.example.ymo.load;
 
 import com.example.ymo.DTO.AnketDTO;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
@@ -27,6 +29,34 @@ public class OtherServices {
         return restTemplate.exchange("http://192.168.11.252:8085/ankets",
                 HttpMethod.GET, request, new ParameterizedTypeReference<List<AnketDTO>>() {
         }).getBody();
+    }
+    public List<AnketDTO> getAnketDTOs2() {
+        System.out.println("Получение анкет");
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        String token = getTokenFromAuthService2();
+        System.out.println("Token:" + token);
+        headers.add("Authorization", "Bearer " + token);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(headers);
+        return restTemplate.exchange("https://ebook.vstu.by/ankets",
+                HttpMethod.GET, request, new ParameterizedTypeReference<List<AnketDTO>>() {
+                }).getBody();
+    }
+    private String getTokenFromAuthService2() {
+        System.out.println("Начало метода получения токена");
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("username", "admin@gmail.com");
+        body.add("password", "admin");
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + "VlNUVV9FTEVDVFJPTklDSk9VUk5BTF9DTElFTlQ6VlNUVV9FTEVDVFJPTklDSk9VUk5BTF9DTElFTlQ=");
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
+        System.out.println("Запрос к авторизации");
+        ResponseEntity<String> response = restTemplate.exchange("https://ebook.vstu.by/authorization/token?grant_type=password",
+                HttpMethod.POST, request, new ParameterizedTypeReference<String>(){});
+        System.out.println("Получение токена");
+        return Objects.requireNonNull(response.getBody()).split("\"")[3];
     }
 
     private String getTokenFromAuthService() {
--- src/main/java/com/example/ymo/service/NagruzkaService.java
+++ src/main/java/com/example/ymo/service/NagruzkaService.java
@@ -41,7 +41,7 @@ public class NagruzkaService {
         System.out.println("111");
         LoadFromFile f = new LoadFromFile();
         System.out.println("222");
-        List<AnketDTO> ankets = otherServices.getAnketDTOs();
+        List<AnketDTO> ankets = otherServices.getAnketDTOs2();
         long i = 1;
         System.out.println("2");
         for (AnketDTO ank : ankets) {
@@ -115,13 +115,13 @@ public class NagruzkaService {
                             n.gek= f.coefGEKZashDipl*ank.getStudentCount();
 
                             nagr.add(n);
-                            n.diplomProekt = f.coefDiplomRecenz*2 ;
-                            nagr.add(n);
-                            if (plan.getGovExam()!=0) {
-
-                                n.gek= f.coefGEKSpec*ank.getStudentCount();
-                                nagr.add(n);
-                            }
+//                            n.diplomProekt = f.coefDiplomRecenz*2 ;
+//                            nagr.add(n);
+//                            if (plan.getGovExam()!=0) {
+//
+//                                n.gek= f.coefGEKSpec*ank.getStudentCount();
+//                                nagr.add(n);
+//                            }
 
 
 
